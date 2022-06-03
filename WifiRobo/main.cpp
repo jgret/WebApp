fdbf473f1fe731a2f1c60a4eb04ea963f8b43c3c
GREIMEL_thomas <thomgreimel@gmail.com>
2022-06-03 11:06:28 +0200

    image

--- WifiRobo/main.cpp
+++ WifiRobo/main.cpp
@@ -12,11 +12,14 @@
 
 #include <QDebug>
 #include "imagestream.h"
+/*
+ * \r\n--123456789000000000000987654321\r\nContent-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n
+ */
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    Widget w;
-    w.show();
+    //Widget w;
+    //w.show();
 
 
     /*
@@ -26,93 +29,115 @@ int main(int argc, char *argv[]) {
     imgstream.openStream("http://172.17.5.241:81/stream");
     */
 
-    QString part_boudary = "";
+    //    QString part_boudary = "";
 
 
-    QFile stream(":/stream/test.mjpg");
-    if (stream.open(QIODevice::ReadOnly)) {
-        QByteArray buffer;
+    //    QFile stream(":/stream/test.mjpg");
+    //    if (stream.open(QIODevice::ReadOnly)) {
+    //        QByteArray buffer;
 
-        int state = 0;
-        int datalen = 0;
-        while (stream.bytesAvailable()) {
+    //        bool end = false;
+    //        int state = 0;
+    //        int post_init_state = 1;
+    //        int datalen = 0;
+    //        while (stream.bytesAvailable() && !end) {
+    //            switch (state) {
 
-            switch (state) {
-            case(0): {
-                buffer.clear();
-                datalen = 0;
-                state = 1;
-            }
-            case(1): {
-                while (stream.bytesAvailable()) {
-                    char c;
-                    int ret = stream.read(&c, 1);
-                    if (ret == 1) {
-                        if (c == '\n' || c == '\r') {
-                            if (buffer.size() > 2) {
-                                part_boudary = buffer.last(buffer.size() - 2); // -2 doesn't matter, test it
-                                buffer.clear();
-                                state = 2;
-                                break;
-                            }
-                        } else {
-                            buffer += c;
-                        }
-                    }
-                }
-                break;
-            }
-            case(2): {
-                static int count;
-                static int part;
-                while (stream.bytesAvailable()) {
-                    char c;
-                    int ret = stream.read(&c, 1);
-                    if (ret == 1) {
-                        if (c == '\n' || c == '\r') {
-                            count++;
-                        } else {
-                            buffer += c;
-                        }
+    //            // reset state machine
+    //            case(0): {
+    //                buffer.clear();
+    //                datalen = 0;
+    //                state = post_init_state;
+    //                break;
+    //            }
 
-                        if (part == 0) {
-                            if (count > 2) {
-                                buffer.clear();
-                                count = 0;
-                                part++;
-                            }
-                        } else if (part == 1) {
-                            if (count > 2) {
-                                QTextStream head(&buffer);
-                                head.read(16);
-                                head >> datalen;
+    //            // read part boundary
+    //            case(1): {
+    //                while (stream.bytesAvailable()) {
+    //                    char c;
+    //                    int ret = stream.read(&c, 1);
+    //                    if (ret == 1) {
+    //                        if (c == '\n' || c == '\r') {
+    //                            if (buffer.size() > 2) {
+    //                                part_boudary = buffer.last(buffer.size() - 2); // -2 doesn't matter, test it
+    //                                qDebug() << "Part Boundary " << part_boudary;
+    //                                stream.read(1);
+    //                                buffer.clear();
+    //                                state = 2;
+    //                                post_init_state = 2;
+    //                                break;
+    //                            }
+    //                        } else {
+    //                            buffer += c; // add part to part_boundary buffer
+    //                        }
+    //                    }
+    //                }
+    //                break;
+    //            }
 
-                                buffer.clear();
-                                state = 3;
-                                count = 0;
-                                part = 0;
-                            }
-                        }
-                    }
-                }
-                break;
-            }
-            case(3): {
-                if (stream.bytesAvailable() >= datalen) {
-                    buffer += stream.read(datalen);
+    //            // read image header (length)
+    //            case(2): {
+    //                static int count;
+    //                static int part;
+    //                while (stream.bytesAvailable()) {
+    //                    char c;
+    //                    int ret = stream.read(&c, 1);
+    //                    if (ret == 1) {
 
-                    QPixmap pixmap;
-                    pixmap.loadFromData(buffer);
-                    qDebug() << "image found";
+    //                        // skip first part
+    //                        if (c == '\n' || c == '\r') {
+    //                            count++;
+    //                        } else {
+    //                            buffer += c;
+    //                        }
 
-                    state = 0;
-                }
-                break;
-            }
-            }
-        }
+    //                        if (part == 0) {
+    //                            if (count > 2) {
+    //                                count = 0;
+    //                                part++;
+    //                            }
+    //                        } else if (part == 1) {
+    //                            if (count > 2) {
+    //                                QTextStream head(&buffer);
+    //                                head.read(16);
+    //                                head >> datalen;
+    //                                qDebug() << "Datalength: " << datalen;
 
-        /*
+    //                                buffer.clear();
+    //                                state = 3;
+    //                                count = 0;
+    //                                part = 0;
+    //                                break;
+    //                            }
+    //                        }
+    //                    }
+    //                }
+    //                break;
+    //            }
+    //            case(3): {
+    //                if (stream.bytesAvailable() >= datalen) {
+    //                    //qDebug() << datalen;
+    //                    buffer += stream.read(datalen);
+
+    //                    //qDebug() << buffer;
+
+    //                    QPixmap pixmap;
+    //                    if (pixmap.loadFromData(buffer)) {
+    //                        qDebug() << "image found";
+    //                    }
+
+    //                    state = 0;
+
+    //                } else {
+    //                    // only file mode
+    //                    end = true;
+    //                }
+    //                break;
+    //            }
+    //            }
+    //        }
+
+    /*
         QString str_img(buffer);
         QStringList parts = str_img.split("\r\n\r\n");
         //qDebug() << str_img.count("\r\n\r\n");
@@ -128,8 +153,41 @@ int main(int argc, char *argv[]) {
         */
 
 
-    } else {
-        qDebug() << "could not open file";
+    //    } else {
+    //        qDebug() << "could not open file";
+    //    }
+
+    QString part_boudary = "";
+
+    QByteArray buffer;
+    QFile stream(":/stream/test.mjpg");
+    if (stream.open(QIODevice::ReadOnly)) {
+
+        buffer += stream.readAll();
+        QString imgdata(buffer);
+
+        QStringList parts = imgdata.split("\r\n\r\n");
+        if (parts.length() > 0) {
+            qDebug() << parts.length();
+            for (QString s : parts) {
+                int len = s.indexOf("\r");
+                QStringList pparts = s.split("\r\n");
+                qDebug() << "pparts.length: " << pparts.length();
+                if (len > 0) {
+                    QByteArray data = s.first(len).toUtf8();
+                    qDebug() << "Header: " << s.last(20);
+                    qDebug() << "Length: " << pparts[0].length();
+                    qDebug() << "First: " << pparts[0].first(20);
+                    qDebug() << "Last: " << pparts[0].last(20);
+                    QPixmap p;
+                    qDebug() << "Image Status: " << p.loadFromData(data);
+                }
+            }
+        }
     }
-    return a.exec();
+    else {
+        qDebug() << "Could not open file";
+    }
+
+    return 0;
 }
